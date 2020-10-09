b47bc327c948fd0e7b10d0425c57266b0f2ffa6f
JINU P R <jinup@JINUs-MacBook-Pro.local>
2020-10-09 20:46:03 +0530

    bit problems

--- trees_and_graphs/route_between_nodes.py
+++ trees_and_graphs/route_between_nodes.py
@@ -5,20 +5,57 @@ Given a directed graph, design an algorithm to find out whether there is a route
 
 
 
-def route_between_nodes(graph, start, end):
+def route_between_nodes_using_dfs(graph, start, end):
 
-	visited = set([start])
-
-	# depth first
+	visited = set()
+	import pdb; pdb.set_trace()
 	def dfs(node):
 		if node == end:
 			return True
-
-		for neigh in graph(node, []):
+		visited.add(node)
+		for neigh in graph.get(node, []):
 			if neigh not in visited:
-				return dfs(neigh)
+				dfs(neigh)
 
-	return dfs(start) if dfs(start) else False
+	return dfs(start)
+
+
+
+	# def dfs_with_stack(node):
+	# 	if node == end:
+	# 		return True
+	# 	stack = [node]
+	# 	visited.add(node)
+	# 	while stack:
+	# 		s = stack.pop(-1)
+	# 		for neigh in graph.get(s, []):
+	# 			if neigh == end:
+	# 				return True
+	# 			if neigh not in visited:
+	# 				stack.append(neigh)
+	# 				visited.add(neigh)
+	# 	return False
+	# return dfs_with_stack(start)
+
+
+def route_between_nodes_using_bfs(graph, start, end):
+	if start == end:
+		return True
+	visited = set()
+	queue = []
+	queue.append(start)
+	while len(queue) > 0:
+		u = queue.pop(0)
+		visited.add(u)
+		for v in graph.get(u, []):
+			if v not in visited:
+				if v == end:
+					return True
+				visited.add(v)
+				queue.append(v)
+	return False
+
+
 
 
 
@@ -29,5 +66,11 @@ graph = {
 	3: [2],
 	4: [1, 3]
 }
+print(route_between_nodes_using_dfs(graph, 0, 3))
+# print(route_between_nodes_using_bfs(graph, 0, 3))
 
-print(route_between_nodes(graph, 4, 2))
+# print(route_between_nodes_using_dfs(graph, 0, 5))
+# print(route_between_nodes_using_bfs(graph, 0, 5))
+
+# print(route_between_nodes_using_dfs(graph, 1, 5))
+# print(route_between_nodes_using_bfs(graph, 1, 5))
